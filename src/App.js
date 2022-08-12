082b6590e64cbfa0fb5bb2256cda07841734d355
oOoRenaudoOo <salliotrenaud@gmail.com>
2022-08-12 06:46:25 -0500

    Afficher-supprimer des images

--- src/App.js
+++ src/App.js
@@ -1,47 +1,83 @@
-// import "./css/App.css";
+// import "./css/App.css"; <= utilisation styles de tailwindcss (index.css)
 
 // composant fonctionnel
-// function App({ title }) {
-//   return <h1 className="text-center text-blue-500 text-3xl">{title}</h1>;
-// }
 
-// composant class
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Picture from "./components/Picture";
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { show: false, title: "Coucou Les amis" };
+function App() {
+  const [title, setTile] = useState("Coucou les amis");
+  const [show, setShow] = useState(false);
+  const isShowInitialise = useRef(false);
 
-    console.log("constructeur appele");
-  }
-
-  componentDidMount() {
+  useEffect(() => {
     console.log("composant App monte");
-    // this.setState({ title: "le composant a bien ete monte" });
+  }, []);
+
+  useEffect(() => {
+    if (isShowInitialise.current) {
+      console.log("show mis a jour");
+    } else {
+      isShowInitialise.current = true;
+    }
+  }, [show]);
+
+  function handleCick() {
+    setShow(!show);
   }
 
-  componentDidUpdate() {
-    console.log("composant App mis a jour");
-  }
-
-  render() {
-    console.log("render appele");
-    return (
-      <div>
-        <h1 className="text-4xl text-purple-700 my-5">{this.state.title}</h1>
-        <button
-          className="bg-purple-900 text-white rounded py-2 px-3"
-          onClick={() => this.setState({ show: !this.state.show })}
-        >
-          Click
-        </button>
-
-        {this.state.show ? <Picture /> : null}
-      </div>
-    );
-  }
+  return (
+    <div>
+      {console.log("render App")}
+      <h1 className="text-4xl text-purple-700 my-5">{title}</h1>
+      <button
+        className="bg-purple-900 text-white rounded py-2 px-3"
+        onClick={handleCick}
+      >
+        Click
+      </button>
+      {show ? <Picture /> : null}
+    </div>
+  );
 }
+
+// class composant
+// import React from "react";
+// import Picture from "./components/Picture";
+
+// class App extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = { show: false, title: "Coucou Les amis" };
+
+//     console.log("constructeur appele");
+//   }
+
+//   componentDidMount() {
+//     console.log("composant App monte");
+//     // this.setState({ title: "le composant a bien ete monte" });
+//   }
+
+//   componentDidUpdate() {
+//     console.log("composant App mis a jour");
+//   }
+
+//   render() {
+//     console.log("render appele");
+//     return (
+//       <div>
+//         <h1 className="text-4xl text-purple-700 my-5">{this.state.title}</h1>
+//         <button
+//           className="bg-purple-900 text-white rounded py-2 px-3"
+//           onClick={() => this.setState({ show: !this.state.show })}
+//         >
+//           Click
+//         </button>
+
+//         {this.state.show ? <Picture /> : null}
+//       </div>
+//     );
+//   }
+// }
 
 export default App;
