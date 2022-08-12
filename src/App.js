59884d743d7f05b5ed4402e3da4dc37f3a70821d
oOoRenaudoOo <salliotrenaud@gmail.com>
2022-08-11 21:22:38 -0500

    React JS - Tutos from 0

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+// import "./css/App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+// composant fonctionnel
+// function App({ title }) {
+//   return <h1 className="text-center text-blue-500 text-3xl">{title}</h1>;
+// }
+
+// composant class
+import React from "react";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { show: false };
+  }
+
+  render() {
+    return (
+      <div>
+        <button
+          class="bg-purple-900 text-white rounded py-2 px-3"
+          onClick={() => this.setState({ show: !this.state.show })}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          Click
+        </button>
+
+        {this.state.show ? (
+          <img class="w-40 mx-auto" src="logo192.png" alt="" />
+        ) : null}
+      </div>
+    );
+  }
 }
 
 export default App;
